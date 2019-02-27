fda0ff515b41df9220c291ac2ac45fbc18c34f74
PauloNobre <paulinho140@gmail.com>
2019-02-27 08:33:04 -0300

    13: Reusando Componentes com Input properties

---
+++ src/app/input-property/input-property.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-input-property',
+  templateUrl: './input-property.component.html',
+  styleUrls: ['./input-property.component.css']//,
+  //inputs: ['nomeCurso':nome']
+})
+export class InputPropertyComponent implements OnInit {
+
+  @Input() nomeCurso: string = '';
+  //Input('nome') nomeCurso: string = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
